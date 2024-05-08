f128358cd9fb012319671b8591423c645f301b26
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-05-08 21:58:48 +0000

    fix code

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    Asynchronous generator that yields a random number between 0 and 10 after waiting for 1 second asynchronously.
+    Asynchronous generator that yields a random number between 0 and 
+    10 after waiting for 1 second asynchronously.
     """
     for i in range(10):
         await sleep(1)
